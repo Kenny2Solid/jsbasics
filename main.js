b47a39d12b5575b44a3ece4e304cd358d2a234d1
Kendrick Torrance <torraken000@warren.k12.in.us>
2024-09-25 07:31:22 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let js= "amazing";
+
+console.log(js);
+
+console.log("Kenny")
+console.log(23);
+
+let x = 2*9
+console.log(x);
+
+let truefalsevalue = true;
+
+let first_name = "kendrick"
+
+let Last_name = "Torrance"
+
+if (js) {
+    console.log(first_name);
+
+else
+    {
+        console.log(Last_name)
+    }
+}
